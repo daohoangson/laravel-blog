fa48451f7bd89b296995d468f98af9a52ade5d25
Dao Hoang Son <daohoangson@gmail.com>
2014-09-29 01:17:12 +0700

    [angularjs] enabled ajax on everypage. Should find a way to avoid the duplicated data request though.

--- app/views/angular.blade.php
+++ app/views/angular.blade.php
@@ -1,16 +1,5 @@
 @extends('layouts.master')
 
 @section('content')
-    <div ng-view>
-        Loading...
-    </div>
-@stop
-
-@section('foot')
-	{{ HTML::script('app/js/angular.min.js') }}
-	{{ HTML::script('app/js/angular-route.min.js') }}
-	{{ HTML::script('app/js/ui-bootstrap-tpls-0.11.0.min.js') }}
-	{{ HTML::script('app/js/services.js') }}
-	{{ HTML::script('app/js/controllers.js') }}
-	{{ HTML::script('app/js/app.js') }}
+    Loading...
 @stop
